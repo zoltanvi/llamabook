fd4fa862013a81d647cef26fed5263d93679dbbe
Attila <konczer2@gmaill.com>
2017-05-17 16:33:25 +0200

    komment

--- src/main/java/llamabook/Main.java
+++ src/main/java/llamabook/Main.java
@@ -1,23 +1,19 @@
 package llamabook;
 
 import llamabook.controller.Controller;
-import llamabook.view.LoginScreen;
-/**
- *
- * @author ShockWave
- *
- */
+
+
 public class Main {
-    public static void main(String[] args) {
-        // Login screen elindítása
-    	
-        startApp();
+	public static void main(String[] args) {
+		// Login screen elindítása
 
-    }
+		startApp();
 
-    private static void startApp() {
-        Controller controller = new Controller();
-	controller.startDesktop();
-    }
+	}
+
+	private static void startApp() {
+		Controller controller = new Controller();
+		controller.startDesktop();
+	}
 
 }
